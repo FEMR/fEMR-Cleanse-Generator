5c6270cf140317bb97b2d40a13a26b03cccc9208
AJ Saclayan <ajsaclayan@gmail.com>
2015-10-19 14:21:51 -0400

    Playing Around with Tables - AJ

---
+++ ADMIN PAGE/test.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost:8080";
+$username = "test";
+$password = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
